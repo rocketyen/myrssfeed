1b0d9b01c420ec7ee00b67774a373318fc991652
rocketyen <rocketyen@gmail.com>
2021-09-27 16:47:51 +0200

    commit

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -1,14 +1,11 @@
 
 <?php
+$error = [];
+$actu = "https://www.jeuxactu.com/rss/tests.rss"; /* insérer ici l'adresse du flux RSS de votre choix */
+$rss = simplexml_load_file($actu);
 
 include('../views/templates/header.php');
-include('../views/parameters.php');
+include('../index.php');
 include('../views/templates/footer.php');
-$error = [];
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-
-
-
-}
 ?>
--- views/pages.php
+++ views/pages.php
@@ -1,9 +1,23 @@
 <?php
 include('../views/templates/header.php');
+include('../class.rss.php');
 ?>
-
-
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="offset-4 col-4 form-label mt-2"><?= $actu ?></div>
+    <div class="offset-4 col-4 form-label mt-2"><?= $test ?></div>
+    <div class="offset-4 col-4 form-label mt-2"><?= $ps5 ?></div>
+    <div class="offset-4 col-4 form-label mt-2"><?= $tips ?></div>
+    <div class="offset-4 col-4 form-label mt-2"><?= $news ?></div>
+</body>
+</html>
 <?php
 include('../views/templates/footer.php');
 ?>
