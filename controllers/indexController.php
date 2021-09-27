e1ce0ac55d5842d1f69174ff1f26c7bb5d75e1b7
rocketyen <rocketyen@gmail.com>
2021-09-27 13:35:30 +0200

    commit

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -1,6 +1,13 @@
 <?php
 
 include('../views/templates/header.php');
+include('../views/parameters.php');
 include('../views/templates/footer.php');
+include('../assets/js/script.js');
+$error = [];
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+}
 
 ?>
---
+++ views/pages.php
@@ -0,0 +1,8 @@
+<?php
+
+include('../views/templates/header.php');
+
+
+include('../views/templates/footer.php');
+
+?>
