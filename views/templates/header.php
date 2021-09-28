5de35861b161a82b3aca5fb99c7ff1fc4cbf69b2
Eric DURIEUX <eric-du@orange.fr>
2021-09-28 09:17:54 +0200

    commit

--- views/templates/header.php
+++ views/templates/header.php
@@ -8,4 +8,59 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>    
+<body>
+<header>
+
+<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-secondary">
+
+  <div class="container">
+    
+    <div class="row">
+
+
+  <!-- Grid column -->
+  <div class="col-md-8 mx-auto">
+    
+  <figure class="figure">
+    <img src="img/ImgHeader.png" class="figure-img img-fluid rounded" alt="Logo HTML w3">
+    <figcaption class="figure-caption text-center">
+ </figure>
+</div>
+
+
+
+<div class="container-fluid">
+<div class="row">
+
+
+<!-- Grid column -->
+<div class="col-md-12 mx-auto">
+
+
+  
+  <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item ">
+        <a class="btn btn-success fs-3 my-2 my-sm-0" href="#">Sujet 1 </a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-success fs-3 my-2 my-sm-0" href="#">Sujet 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn btn-success fs-3 my-2 my-sm-0" href="#">Sujet 3</a>
+      </li>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-success fs-3 my-2 my-sm-0" type="button">Commentaires</button>
+    </form>
+  </div>
+</nav>
+</div> 
+</div>      
+</header>
