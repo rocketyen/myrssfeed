76fbc90765fdce0e269b91b0ee121265d25ea0b4
rocketyen <rocketyen@gmail.com>
2021-09-28 13:51:04 +0200

    commit

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,19 +1,54 @@
 
 <?php
-
 $error = [];
+$flux[0] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+echo '<div class="container"><div class="row"><div class="col-4"><ul>';
+
+foreach ($rss->channel->image as $item1) {
+    echo '<li>' . $item1->title . '<a href="' . $item1->link . '" target="blank"><img src="' . $item1->url . '"></a> </li>';
+}
+
+foreach ($rss->channel->item as $item) {
+    $datetime = date_create($item->date);
+    $date = date_format($datetime, 'd M Y, H\hi');
+
+    echo '<li><a href="' . $item->link . '" target="blank">' . $item->title . '</a> (' . $date . ')' . $item->description . '</li>';
+    echo '<img src="' . $item->enclosure['url'] . '">';
+}
+echo '</ul>';
+echo '</div>';
+
+echo '<div class="col-4">';
+echo '<ul>';
+foreach ($rss2->channel->image as $item1) {
+    echo '<li>' . $item1->title . '<a href="' . $item1->link . '" target="blank"><img src="' . $item1->url . '"></a> </li>';
+}
+
+foreach ($rss2->channel->item as $item) {
+    $datetime = date_create($item->date);
+    $date = date_format($datetime, 'd M Y, H\hi');
+
+    echo '<li><a href="' . $item->link . '" target="blank">' . $item->title . '</a> (' . $date . ')' . $item->description . '</li>';
+    echo '<img src="' . $item->enclosure['url'] . '">';
+}
+echo '</ul>';
+echo '</div>';
 
 
+echo '<div class="col-4"><ul>';
+foreach ($rss3->channel->image as $item1) {
+    echo '<li>' . $item1->title . '<a href="' . $item1->link . '" target="blank"><img src="' . $item1->url . '"></a> </li>';
+}
 
+foreach ($rss3->channel->item as $item) {
+    $datetime = date_create($item->date);
+    $date = date_format($datetime, 'd M Y, H\hi');
 
-
-
-
-
+    echo '<li><a href="' . $item->link . '" target="blank">' . $item->title . '</a> (' . $date . ')' . $item->description . '</li>';
+    echo '<img src="' . $item->enclosure['url'] . '">';
+}
+echo '</ul></div></div>';
 include('../views/templates/header.php');
 include('../views/home.php');
 include('../views/templates/footer.php');
-
-
-
 ?>
--- controllers/parametersController.php
+++ controllers/parametersController.php
@@ -5,13 +5,17 @@ include('../views/templates/header.php');
 include('../views/parameters.php');
 include('../views/templates/footer.php');
 
-// if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $flux[0] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+    $flux[1] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+    $flux[2] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+    $flux[3] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+    $flux[4] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
+    setcookie($flux[]);
+} 
 // $darkMode = intval(trim(filter_input(INPUT_POST, 'darkMode', FILTER_SANITIZE_NUMBER_INT)));
 // $nbNews = intval(trim(filter_input(INPUT_POST, 'nbNews', FILTER_SANITIZE_NUMBER_INT)));
 // $flux = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
-// setcookie($flux[]);
-
 // if($darkMode!=1 && $darkMode !=0){
 //     $error
 // }
