8dd184a74d72d2b9eb0c1deb18f35a799b577d43
Julien <julien.darras12@gmail.com>
2021-09-28 12:08:37 +0200

    home col

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -7,9 +7,7 @@ $error = [];
 
 
 
-$rss = simplexml_load_file($flux[0]);
-$rss2 = simplexml_load_file($flux[1]);
-$rss3 = simplexml_load_file($flux[2]);
+
 
 
 include('../views/templates/header.php');
