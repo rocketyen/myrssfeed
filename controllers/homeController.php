a63b0ce15ee8808a57535455c759b4145ea0b8ba
dylan <dylan.trimouille@gmx.fr>
2021-09-29 09:34:07 +0200

    modified

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,10 +2,10 @@
 <?php
 
 require_once(dirname(__FILE__).'/../assets/config/config.php');
-include(dirname(__FILE__).'/generalController.php');
 $error = [];
 $flux[0] = filter_input(INPUT_POST, 'flux', FILTER_REQUIRE_ARRAY);
 
+include(dirname(__FILE__).'/generalController.php');
 include(dirname(__FILE__).'/../views/templates/header.php');
 include(dirname(__FILE__).'/../views/home.php');
 include(dirname(__FILE__).'/../views/templates/footer.php');
--- controllers/pagesController.php
+++ controllers/pagesController.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(dirname(__FILE__).'/generalController.php');
 include(dirname(__FILE__).'/../views/templates/header.php');
 include(dirname(__FILE__).'/../views/pages.php');
 include(dirname(__FILE__).'/../views/templates/footer.php');
